155b69fbe5b6cbbab58d30a077aea3b543443b66
cgordon0 <cgordon0@byu.edu>
2026-01-23 09:43:44 -0700

    Fixing King

--- shared/src/main/java/chess/pieces/King.java
+++ shared/src/main/java/chess/pieces/King.java
@@ -13,17 +13,14 @@ public class King implements AbstractPiece {
                 {-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}
         };
 
-        for (int[] d : adjacent) {
-            ChessPosition newPosition = new ChessPosition(
-                    myPosition.getRow() + d[0],
-                    myPosition.getColumn() + d[1]
-            );
+        for (int[] i : adjacent) {
+            ChessPosition newPosition = new ChessPosition(myPosition.getRow() + i[0], myPosition.getColumn() + i[1]);
 
-            if (!newPosition.insideBoard()) continue;
+            if (!newPosition.insideBoard()) {
+                continue;
+            }
 
-            if (board.getPiece(newPosition) != null &&
-                    board.getPiece(myPosition).getTeamColor() ==
-                            board.getPiece(newPosition).getTeamColor()) {
+            if (board.getPiece(newPosition) != null && board.getPiece(myPosition).getTeamColor() == board.getPiece(newPosition).getTeamColor()) {
                 continue;
             }
 
