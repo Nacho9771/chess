d4542f433e0fc7ac3fb5763de1e64379324ef340
cgordon0 <cgordon0@byu.edu>
2026-01-27 13:13:55 -0700

    Cleaning up Pawn

--- shared/src/main/java/chess/pieces/Pawn.java
+++ shared/src/main/java/chess/pieces/Pawn.java
@@ -1,5 +1,4 @@
 package chess.pieces;
-
 import java.util.ArrayList;
 import chess.*;
 
@@ -7,7 +6,6 @@ public class Pawn implements AbstractPiece {
 
     @Override
     public ArrayList<ChessMove> pieceMoves(ChessBoard board, ChessPosition start) {
-
         ArrayList<ChessMove> moves = new ArrayList<>();
 
         ChessPiece pawn = board.getPiece(start);
@@ -15,23 +13,18 @@ public class Pawn implements AbstractPiece {
         int startRow = (direction == 1) ? 2 : 7;
         int promotionRow = (direction == 1) ? 8 : 1;
 
-        // Forward 1
         ChessPosition forward1 = new ChessPosition(start.getRow() + direction, start.getColumn());
 
         if (forward1.insideBoard() && board.getPiece(forward1) == null) {
             moves.add(new ChessMove(start, forward1, null));
 
-            // Forward 2
             ChessPosition forward2 = new ChessPosition(start.getRow() + 2 * direction, start.getColumn());
-
             if (start.getRow() == startRow && board.getPiece(forward2) == null) {
                 moves.add(new ChessMove(start, forward2, null));
             }
         }
 
-        // Diagonal
         ChessPosition leftCapture = new ChessPosition(start.getRow() + direction, start.getColumn() - 1);
-
         ChessPosition rightCapture = new ChessPosition(start.getRow() + direction, start.getColumn() + 1);
 
         if (leftCapture.insideBoard()) {
@@ -40,7 +33,6 @@ public class Pawn implements AbstractPiece {
                 moves.add(new ChessMove(start, leftCapture, null));
             }
         }
-
         if (rightCapture.insideBoard()) {
             ChessPiece rightPiece = board.getPiece(rightCapture);
             if (rightPiece != null && rightPiece.getTeamColor() != pawn.getTeamColor()) {
@@ -48,7 +40,6 @@ public class Pawn implements AbstractPiece {
             }
         }
 
-        // Promotions
         ArrayList<ChessMove> finalMoves = new ArrayList<>();
 
         for (ChessMove move : moves) {
@@ -63,7 +54,6 @@ public class Pawn implements AbstractPiece {
                 finalMoves.add(move);
             }
         }
-
         return finalMoves;
     }
 }
