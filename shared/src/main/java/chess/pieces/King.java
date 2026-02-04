e61924203e85e6fd0772d378e1859f5cf7b1fa16
cgordon0 <cgordon0@byu.edu>
2026-02-04 09:32:14 -0700

    Cleaning King

--- shared/src/main/java/chess/pieces/King.java
+++ shared/src/main/java/chess/pieces/King.java
@@ -8,16 +8,12 @@ public class King implements AbstractPiece {
     public ArrayList<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ArrayList<ChessMove> possibleMoves = new ArrayList<>();
 
-        int[][] adjacent = {
-                {-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1}, {0,-1}
-        };
+        int[][] adjacent = {{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1}, {0,-1}};
 
         for (int[] i : adjacent) {
             ChessPosition newPosition = new ChessPosition(myPosition.getRow() + i[0], myPosition.getColumn() + i[1]);
 
-            if (!newPosition.insideBoard()) {
-                continue;
-            }
+            if (!newPosition.insideBoard()) {continue;}
 
             if (board.getPiece(newPosition) != null && board.getPiece(myPosition).getTeamColor() == board.getPiece(newPosition).getTeamColor()) {
                 continue;
