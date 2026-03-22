3ca0b26fdafc7a7afbed4ad4874072021bbff6bd
Connor Gordon <cgordon0@byu.edu>
2026-03-22 09:42:43 -0600

    Make tests for serverfacade

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,29 +1,54 @@
 package client;
 
-import org.junit.jupiter.api.*;
+import model.AuthData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import server.Server;
 
+import java.util.List;
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
-    @BeforeAll
     public static void init() {
         server = new Server();
-        var port = server.run(0);
+        int port = server.run(0);
+        facade = new ServerFacade(port);
         System.out.println("Started test HTTP server on " + port);
     }
 
-    @AfterAll
+    void clearDatabase() throws Exception {
+        facade.clear();
+    }
+
     static void stopServer() {
         server.stop();
     }
 
-
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void clearPositive() {
+        Assertions.assertDoesNotThrow(() -> facade.clear());
     }
 
+    @Test
+    void clearNegativeBadServer() {
+        ServerFacade badFacade = new ServerFacade("http://localhost:67676767");
+
+        ServerFacadeException ex = Assertions.assertThrows(ServerFacadeException.class, badFacade::clear);
+        Assertions.assertEquals("Error: unable to reach server", ex.getMessage());
+    }
+
+    @Test
+    void clearPositiveRemovesData() throws Exception {
+        AuthData auth = facade.register("clear-user", "password", "clear@email.com");
+        facade.createGame(auth.authToken(), "before-clear");
+
+        facade.clear();
+
+        AuthData secondAuth = facade.register("clear-user", "password", "clear@email.com");
+        List<GameSummary> games = facade.listGames(secondAuth.authToken());
+        Assertions.assertTrue(games.isEmpty());
+    }
 }
